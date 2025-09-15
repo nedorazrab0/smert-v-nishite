48bc6bee25a8b3b2f5abcf007b5340eaff29cb7e
valenok <141725500+nedorazrab0@users.noreply.github.com>
2025-09-15 21:06:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 
 from PIL import Image, ImageDraw, ImageFont
 
-image = Image.open("./template.png")
+img = Image.open("./template.png")
+y = img.height/1.9
 draw = ImageDraw.Draw(image)
-y = image.height/1.9
 font = ImageFont.truetype("./lobster.ttf", 54)
 
 first = ["Смерть", "Богатство", "Долги", "Дипрессия", "Репрессии", "Эфтаназия",
@@ -25,11 +25,12 @@ second = ["сталина", "жирной коровы", "тиринга", "ог
           "сеги", "геншина", "дон", "малоежки", "духоты", "крокодила",
           "доширака", "лечения рака", "1 egg", "долгов", "алиментов", "аниме"]
 
-4244389328135950139635730789338505517608545448571440746862351343433371972850356
+i = 0
 for a in first:
     for b in second:
-        text = first[a] + " " + second[b]
+        i += 1
+        text = first[a] + " " + "от" + " " + second[b]
         text_length = draw.textlength(text, font)
         x = (image.width - text_length)/2
         draw.text((x, y), text, font=font)
-        image.save("o.webp", "webp", optimize = True, quality = 0.01)
+        img.save(f"./images/{i}.webp", "webp", optimize = True, quality = 0.01)
