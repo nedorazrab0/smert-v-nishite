4209c461aebd5cc4f1566367a9506a318d0a3994
valenok <141725500+nedorazrab0@users.noreply.github.com>
2025-09-15 19:03:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ draw = ImageDraw.Draw(image)
 y = image.height/1.9
 font = ImageFont.truetype("./lobster.ttf", 54)
 
-
-text = ""
-text_length = draw.textlength(text, font)
-x = (image.width - text_length)/2
-draw.text((x, y), text, font=font)
-image.save("o.webp", "webp", optimize = True, quality = 0.01)
+for a in first:
+    for b in second:
+        text = first[a] + " " + second[b]
+        text_length = draw.textlength(text, font)
+        x = (image.width - text_length)/2
+        draw.text((x, y), text, font=font)
+        image.save("o.webp", "webp", optimize = True, quality = 0.01)
